5bfcf72f49c1bb5beb1cb86d7ab3f34f4ffc2ce7
UnicornSang <ssxtsang@gmail.com>
2025-07-23 16:41:59 +0700

    list quan tri

--- books_nest/admin/views/taikhoan/quantri/addQuanTri.php
+++ books_nest/admin/views/taikhoan/quantri/addQuanTri.php
@@ -10,72 +10,72 @@
 <!-- end-navbar -->
 
 <!-- Main Sidebar Container -->
-<?php include './views/layout/sidebar.php';?>
+<?php include './views/layout/sidebar.php'; ?>
 
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Quản lý tài khoản quản trị viên</h1>
-        </div>
-         </div>
-          </div>
-  </section>
-         <section class="content">  
-
-         <div class="container-fluid">
-
-         <div class="row">
-          <div class="col-12">
-            <div class="card card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Thêm quản trị</h3>
-              </div>
-              <!-- /.card-header -->
-              <!-- form start -->
-              <form action="<?= BASE_URL_ADMIN. '?act=them-danh-muc'?>" method="POST">
-                <div class="card-body">
-                  <div class="form-group ">
-                    <label >Họ tên</label>
-                    <input type="text" class="form-control" name="ho_ten"  placeholder="nhập họ tên">
-                    <?php if (isset($_SESSION['error']['ho_ten'])) {  ?>
-             
-
-                    <p class="text-danger"><?=$_SESSION['error']['ho_ten'] ?></p>
-
-                    <?php } ?>
-                    <div class="form-group ">
-                    <label >Email</label>
-                    <input type="email" class="form-control" name="email"  placeholder="nhập email">
-                    <?php if (isset($_SESSION['error']['email'])) {  ?>
-             
-
-                    <p class="text-danger"><?=$_SESSION['error']['email'] ?></p>
-
-                    <?php } ?>
-                <!-- /.card-body -->
-
-                <div class="card-footer">
-                  <button type="submit" class="btn btn-primary">Submit</button>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Quản lý tài khoản quản trị viên</h1>
                 </div>
-              </form>
             </div>
-          </div>
-         </div>
-         </div>
-          
-         
+        </div>
+    </section>
+    <section class="content">
 
-         </section>
+        <div class="container-fluid">
 
-<!-- Footer -->
-<?php include './views/layout/footer.php'; ?>
-<!-- end footer -->
+            <div class="row">
+                <div class="col-12">
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Thêm quản trị</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <!-- form start -->
+                        <form action="<?= BASE_URL_ADMIN . '?act=them-danh-muc' ?>" method="POST">
+                            <div class="card-body">
+                                <div class="form-group ">
+                                    <label>Họ tên</label>
+                                    <input type="text" class="form-control" name="ho_ten" placeholder="nhập họ tên">
+                                    <?php if (isset($_SESSION['error']['ho_ten'])) {  ?>
 
 
-</body>
+                                        <p class="text-danger"><?= $_SESSION['error']['ho_ten'] ?></p>
 
-</html>
+                                    <?php } ?>
+                                    <div class="form-group ">
+                                        <label>Email</label>
+                                        <input type="email" class="form-control" name="email" placeholder="nhập email">
+                                        <?php if (isset($_SESSION['error']['email'])) {  ?>
+
+
+                                            <p class="text-danger"><?= $_SESSION['error']['email'] ?></p>
+
+                                        <?php } ?>
+                                        <!-- /.card-body -->
+
+                                        <div class="card-footer">
+                                            <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </section>
+
+    <!-- Footer -->
+    <?php include './views/layout/footer.php'; ?>
+    <!-- end footer -->
+
+
+    </body>
+
+    </html>
